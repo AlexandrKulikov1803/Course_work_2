96c1923fa87b5fcc96bf843dc4f73dcea3339301
Alexandr Kulikov <kulic1803@yandex.ru>
2025-09-03 02:00:05 +0600

    Adding tests for class TXTWriter

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 from src.head_hunter_api import HeadHunterAPI
+from src.json_writer import JSONWriter
+from src.text_writer import TXTWriter
 from src.vacancy import Vacancy
 
 # Создание экземпляра класса для работы с API сайтов с вакансиями
 hh_api = HeadHunterAPI()
 
 # Получение вакансий с hh.ru в формате JSON
-hh_vacancies = hh_api.get_vacancies("Python")
+hh_vacancies = hh_api.get_vacancies("Python", 3)
 
 # Преобразование набора данных из JSON в список объектов
 vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
 
 # Пример работы конструктора класса с одной вакансией
-vacancy = Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", 100000, "Требования: опыт работы от 3 лет...")
+vacancy = Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", 100000, "Опыт работы от 3 лет")
 
 
-# Сохранение информации о вакансиях в файл
+# Сохранение информации о вакансиях в json-файл
 json_saver = JSONWriter()
-# json_saver.add_vacancy(vacancy)
-# json_saver.delete_vacancy(vacancy)
-#
-#
+json_saver.add_data(vacancy)
+json_saver.del_data(vacancy)
+
+
+# Сохранение информации о вакансиях в txt-файл
+txt_saver = TXTWriter()
+txt_saver.add_data(vacancy)
+txt_saver.del_data(vacancy)
+
+
 # # Функция для взаимодействия с пользователем
 # def user_interaction():
 #     platforms = ["HeadHunter"]
